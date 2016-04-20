253b620f4893f7b7b5a7a60938ccfdc81f10ecb0
evan <evan_jw@yahoo.com>
2016-04-20 13:02:35 +1000

    Added search function

--- js/Controller/TransactionController.js
+++ js/Controller/TransactionController.js
@@ -7,12 +7,48 @@ var TransactionController= function($scope){
 	
 	var dummyData1 = [
 		{
-			Date: '01/01/2016',
-			Description: 'Transaction in new year',
-			Transaction: '-$50.31',
-			Balance: '50'
+			Date: '21/04/2016',
+			Description: 'Buying dinner',
+			Transaction: '-$15.00',
+			Balance: '$6105.00'
+		},
+		{
+			Date: '19/04/2016',
+			Description: 'Win a bet',
+			Transaction: '$20.00',
+			Balance: '$6120.00'
+		},
+		{
+			Date: '10/04/2016',
+			Description: 'Salary from xxxxxxxx',
+			Transaction: '$2000.00',
+			Balance: '$6100.00'
+		},
+		{
+			Date: '09/04/2016',
+			Description: 'Give donation for world peace',
+			Transaction: '-$50.00',
+			Balance: '$4100.00'
+		},
+		{
+			Date: '01/04/2016',
+			Description: 'Buying clothes',
+			Transaction: '-$150.00',
+			Balance: '$4150.00'
+		},
+		{
+			Date: '25/03/2016',
+			Description: 'Buying groceries',
+			Transaction: '-$50.00',
+			Balance: '$4300.00'
 		}
 	];
 	
 	$scope.transactions = dummyData1;
+	
+	$scope.searchDesc = function(){
+		$scope.transactions = dummyData1;
+		var searchResult = $.grep($scope.transactions, function (item) { return item.Description.toLowerCase().indexOf($scope.searchTerm) >= 0 });
+		$scope.transactions = searchResult;
+	}
 }
